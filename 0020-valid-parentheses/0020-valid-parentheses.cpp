72ff1b25663f7302a486bd82297469a31c5e2bf7
komalkvy <komal969360@gmail.com>
2025-04-05 17:34:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char> st;
+    for (char ch : s)
+    {
+        if (ch == '(' || ch =='{' || ch == '[')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if (!st.empty())
+            {
+                if (ch == ')' && st.top() == '(' || ch == '}' && st.top() == '{' || ch == ']' && st.top() == '[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    if (st.empty())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }   
+    }
+};
